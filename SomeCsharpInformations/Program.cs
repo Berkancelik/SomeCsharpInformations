5814b1155b5ff40f71b9d44134b6d370f7fef583
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-21 09:37:37 +0300

    Add project files.

---
+++ SomeCsharpInformations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeCsharpInformations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
