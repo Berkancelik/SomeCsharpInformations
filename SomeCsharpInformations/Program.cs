9f56a0ff38950e756e552eaae4e6364e33d9dbb0
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-21 10:50:42 +0300

    Delegate

--- SomeCsharpInformations/Program.cs
+++ SomeCsharpInformations/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SomeCsharpInformations
 {
     public delegate void FullNameDelegate(string name, string surname);
+
+
+
+
+
     internal class Program
     {
         static void Main(string[] args)
@@ -11,21 +17,17 @@ namespace SomeCsharpInformations
             FullNameDelegate fullNameDelegate2 = new FullNameDelegate(FullNameMethod2);
             FullNameDelegate fullNameDelegate3 = new FullNameDelegate(FullNameMethod3);
 
-            FullNameDelegate multipleDelegate = fullNameDelegate1 + fullNameDelegate2+ fullNameDelegate3;
-            multipleDelegate("Berkan", "Çelik");
-            Console.WriteLine(multipleDelegate);
+            
         }
 
         public static void FullNameMethod1(string name, string surName)
         {
             Console.WriteLine(name + " " + surName);
         }
-
         public static void FullNameMethod2(string  name, string surName)
         {
             Console.WriteLine(name.ToUpper()+ " " + surName.ToUpper());
         }
-
         public static void FullNameMethod3(string name, string surName)
         {
             Console.WriteLine(name.ToUpper() + " " + surName.ToUpper());
