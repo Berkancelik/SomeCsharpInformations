d117a53cbb5e74089bb458b4f36f24ab325e024d
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-21 09:56:31 +0300

    SingleDelegate&MultipleDelegate

--- SomeCsharpInformations/Program.cs
+++ SomeCsharpInformations/Program.cs
@@ -2,11 +2,34 @@
 
 namespace SomeCsharpInformations
 {
+    public delegate void FullNameDelegate(string name, string surname);
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FullNameDelegate fullNameDelegate1 = new FullNameDelegate(FullNameMethod1);
+            FullNameDelegate fullNameDelegate2 = new FullNameDelegate(FullNameMethod2);
+            FullNameDelegate fullNameDelegate3 = new FullNameDelegate(FullNameMethod3);
+
+            FullNameDelegate multipleDelegate = fullNameDelegate1 + fullNameDelegate2+ fullNameDelegate3;
+            multipleDelegate("Berkan", "Çelik");
+            Console.WriteLine(multipleDelegate);
+        }
+
+        public static void FullNameMethod1(string name, string surName)
+        {
+            Console.WriteLine(name + " " + surName);
+        }
+
+        public static void FullNameMethod2(string  name, string surName)
+        {
+            Console.WriteLine(name.ToUpper()+ " " + surName.ToUpper());
+        }
+
+        public static void FullNameMethod3(string name, string surName)
+        {
+            Console.WriteLine(name.ToUpper() + " " + surName.ToUpper());
         }
     }
 }
+
